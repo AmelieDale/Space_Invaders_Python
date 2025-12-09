b30d704b2f78c6b71405d65dd71450d987cc12ce
HollyDBNU <22503136@bnu.ac.uk>
2025-12-09 10:16:07 +0000

    Adding Mystery Ship

--- alien.py
+++ alien.py
@@ -13,3 +13,25 @@ class Alien(pygame.sprite.Sprite):
 
     def update(self, direction):
         self.rect.x += direction
+
+class MysteryShip(pygame.sprite.Sprite):
+	def __init__(self, screen_width, offset):
+		super().__init__()
+		self.screen_width = screen_width
+		self.offset = offset
+		self.image = pygame.image.load("mystery.png")
+
+		x = random.choice([self.offset/2, self.screen_width + self.offset - self.image.get_width()])
+		if x == self.offset/2:
+			self.speed = 3
+		else:
+			self.speed = -3
+
+		self.rect = self.image.get_rect(topleft = (x, 90))
+
+	def update(self):
+		self.rect.x += self.speed
+		if self.rect.right > self.screen_width + self.offset/2:
+			self.kill()
+		elif self.rect.left < self.offset/2:
+			self.kill()
